de6d531c99abd59d2511a2192f2ab23077fe4ba7
Ritesh Sinha <kumarris@usc.edu>
2018-03-26 00:48:21 -0700

    added , seperator for reduce output

--- aadharanalysis/src/main/java/com/hadoop/aadhar/mapper/SortMapper.java
+++ aadharanalysis/src/main/java/com/hadoop/aadhar/mapper/SortMapper.java
@@ -22,7 +22,7 @@ public class SortMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
      * @throws InterruptedException
      */
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String dataFromReducer[] = value.toString().split("\t");
+        String dataFromReducer[] = value.toString().split(",");
         STATE.set(dataFromReducer[0].trim());
         COUNT.set(Integer.parseInt(dataFromReducer[1].trim()));
         context.write(COUNT, STATE);
